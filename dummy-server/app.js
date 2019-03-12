d4870ae974f39314de07eb50e3c25b5c01fcb8db
KearabiloeNare <39116703+KearabiloeNare@users.noreply.github.com>
2019-03-12 22:15:52 +0200

    Update app.js
    fixed the errors from before ... this should work properly now

--- dummy-server/app.js
+++ dummy-server/app.js
@@ -55,25 +55,27 @@ standard_input.on('data', function (data)
         }
         if(data == 2)
         {
-          //add entry to data base
-           //add entry to data base
-          //delete entries 
-          //copy data to new table the delete from current table
-
           function remove(uID)
           {
-            db.run('SELECT userId FROM Clients Where userId =? ',[uID],function(err) {
+             db.run('CREATE TABLE IF NOT EXISTS Delete_Clients (userId NVARCHAR(50) NOT NULL, Name NVARCHAR(50) NULL,[E-mail] NVARCHAR(50) NULL,Password NVARCHAR(50) NULL)');
+            let sql= `SELECT userId,Name,Surname,[E-mail],Password FROM Clients WHERE userId=?`;
+            db.get(sql, [uID], (err, row) => {
+               if (err) {
+              return console.error(err.message);
+              }
+             
+              return row? console.log(row.userId+ "\t" +row.Name+ "\t" +row.Surname+ "\t" +row['E-mail']+ "\t" +row.Password): console.log(`No client found with the id ${uID}`);
+          db.run(`INSERT INTO Delete_Clients(userId,Name,Surname,[E-mail],Password) VALUES(?,?,?,?,?)`, [row.userId,row.Name,row.Surname,row['E-mail'],row.Password], function(err) {
                    if (err) {return console.log(err.message);}
                        });
+          });
+                         
+            db.run('DELETE FROM Clients WHERE userId=?',[uID],function(err) {
+                   if (err) {     return console.log(err.message);}
+                       });
 
-
-            let sql = 'SELECT userId FROM Clients Where userId =(?) ';
-            db.all(sql, [uID], (err, rows) => { if (err) {
-    throw err;
-  }
-  rows.forEach((row) => {
-    console.log(row.Name);
-  });
+            
+          }
         }
         if(data == 3)
         {
