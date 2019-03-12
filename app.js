c3539c5dd7346f12eda8037a4958f683537964e1
EiselenR <48208268+EiselenR@users.noreply.github.com>
2019-03-12 15:50:56 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+const sqlite3 = require('sqlite3').verbose();
+var express = require("express");
+var app = express();
+var server = require('http').createServer(app);
+const readline = require('readline');
+app.disable('etag');
+
+//port to listen on
+
+  var PORT = 8080;
+  server.listen(PORT);
+
+// open database in memory
+let db = new sqlite3.Database('../database/merlotInfoSys.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+  });
+
+
+//create input/output interface for terminal
+  const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+  // Get process.stdin as the standard input object.
+var standard_input = process.stdin;
+standard_input.setEncoding('utf-8');
+
+        console.log("Your options are the following :");
+        console.log("Option 1 : add a new entry");
+        console.log("Option 2 : delete a entry");
+        console.log("Option 3 : search entries");
+        console.log("Option 4 : List entries");
+        console.log("Option 5 : quit");
+
+
+// When user input data and click enter key.
+standard_input.on('data', function (data) 
+{
+    
+        
+        if(data == 1)
+        {
+          console.log("Option 1 chosen");
+        }
+        if(data == 2)
+        {
+          //add entry to data base
+        }
+        if(data == 3)
+        {
+          //add entry to data base
+        }
+        if(data == 4)
+        {
+          //add entry to data base
+        }
+    // User input exit.
+    if(data === 5){
+        // Program exit.
+        console.log("User input complete, program exit.");
+        db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
+
+        process.exit();
+    }
+    
+});
+
+
+//For debugging test only
+db.serialize(() => {
+    db.each(`SELECT userId as id,
+                    Name as name
+             FROM Clients`, (err, row) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log(row.id + "\t" + row.name);
+    });
+  });
+
+  
+// close the database connection
+
