30fbfce0457f97070904a045e18b296bee09acdb
EiselenR <48208268+EiselenR@users.noreply.github.com>
2019-03-13 10:46:54 +0200

    Add files via upload

---
+++ dummy-server/terminal.js
@@ -0,0 +1,102 @@
+
+var database = require("./database.js");
+exports.AllowInput= function(){
+	//Allow the terminal to multitask by creating another process decicated to listening for commands
+	var stdin = process.openStdin();
+
+
+	stdin.addListener("data", function(d) {
+	  	var commands = d.toString().trim();
+	  	commands = commands.split(" ");
+	  	if(commands.length == 1)
+	  		output= command(commands)
+	  	else
+	  		output = command(commands);
+	  	if(output == false){
+	  		console.log(false);
+	  	}
+	});
+
+function command(command){
+	var cmd = command[0];
+	switch(cmd){
+		
+		
+		case "5":
+		case "exit":{
+			stdin.removeAllListeners('data');
+			console.log("============ Stopping Server ============");
+			process.exit();
+			break;
+		}
+			
+			
+		case "1":
+		case "add":{
+			if(command.length == 6 )
+			{
+				database.insert(command[1],command[2],command[3],command[4],command[5]);
+			}
+			else{
+				console.log("Invalid arguements")
+				console.log("add <userID> <name> <surname> <email> <pass>");
+				
+			}
+
+			break;
+		}
+			
+		case "2":
+		case "del":{
+			if(command.length == 2 )
+			{
+				database.remove(command[1]);
+			}
+			else{
+				console.log("Invalid arguements")
+				console.log("del <userID>");
+			}
+			break;
+		}
+			
+		case "3":
+		case "search":{
+			if(command.length == 2 )
+			{
+				database.Search(command[1]);
+			}
+			else{
+				console.log("Invalid arguements")
+				console.log("search <userID> ");
+			}
+			break;
+		}
+			
+		case "4":
+		case "list":{
+			if(command.length == 1 )
+			{
+				database.Display();
+			}
+			else{
+				console.log("Invalid arguements")
+				console.log("list Entries");
+			}
+			break;
+		}
+		default:{
+			console.log("============ Commands ============");
+			console.log("1) add <userID> <name> <surname> <email> <pass>");
+			console.log("2) del <userID> ");
+			console.log("3) search <userID> ");
+			console.log("4) list");
+			console.log("5) exit");
+			console.log("");
+
+
+
+		}
+	}
+}
+}
+
