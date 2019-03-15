3a9dc42280971ea80873c354c5741313786cd843
Mpho Mashaba <36999004+MphoMashaba@users.noreply.github.com>
2019-03-15 12:57:46 +0200

    Update terminal.js

--- dummy-server/terminal.js
+++ dummy-server/terminal.js
@@ -117,6 +117,18 @@ function command(command){
 						 }
 			 break;
 		}
+		case "8":
+		case "ListT":{
+			if(command.length == 1 )
+			{
+				database.DisplayAuditsTimeSpan();
+			}
+			else{
+				console.log("Invalid arguments for list Entries")
+				}
+			break;
+		}	
+			
 
 		default:{
 			console.log("============ Commands ============");
@@ -127,7 +139,8 @@ function command(command){
 			console.log("5) updateEmail <newEmail> <userId>");
 			console.log("6) updatePassword <newPassword> <userId>");
 			console.log("7) list deleted table")
-			console.log("8) Exit")
+			console.log("8) List audit timespan");
+			console.log("9) Exit");
 			console.log("");
 
 
