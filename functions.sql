9cfa85ba20c7490730b0c3bf1644070b31e2912b
EiselenR <48208268+EiselenR@users.noreply.github.com>
2019-03-27 12:33:48 +0200

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -2,7 +2,7 @@ Drop  Table Clients;
 
 Create table Clients(
 	ClientID serial primary key,
-	"Name" character varying (50) NOT NULL,
+	"name" character varying (50) NOT NULL,
 	"Surname" character varying(50)NOT NULL,
 	"Email" character varying(75)NOT NULL,
 	"PhoneNumber" character varying(20)NOT NULL,
